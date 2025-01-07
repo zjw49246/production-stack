68861d6250289824eb78f85ea6182a9a6f660709
ApostaC <jc4xvyp@outlook.com>
2025-01-07 20:58:52 +0000

    [Add] engine status scraper

---
+++ router/engine_stats.py
@@ -0,0 +1,157 @@
+from typing import Dict
+import threading
+import requests
+from dataclasses import dataclass
+from prometheus_client.parser import text_string_to_metric_families
+
+from service_discovery import GetServiceDiscovery
+from log import init_logger
+logger = init_logger(__name__)
+
+_global_engine_stats_scraper: "Optional[EngineStatsScraper]" = None
+
+@dataclass
+class EngineStats:
+    # Number of running requests
+    num_running_requests: int = 0
+
+    # Number of queuing requests
+    num_queuing_requests: int = 0
+
+    @staticmethod
+    def FromVllmScrape(vllm_scrape: str):
+        """
+        Parse the vllm scrape string and return a EngineStats object
+
+        Args:
+            vllm_scrape (str): The vllm scrape string
+
+        Returns:
+            EngineStats: The EngineStats object
+
+        Note:
+            Assume vllm only runs a single model
+        """
+        num_running_reqs = 0
+        num_queuing_reqs = 0
+        for family in text_string_to_metric_families(vllm_scrape):
+            for sample in family.samples:
+                if sample.name == 'vllm:num_requests_running':
+                    num_running_reqs = sample.value
+                elif sample.name == 'vllm:num_requests_waiting':
+                    num_queuing_reqs = sample.value
+
+        return EngineStats(
+            num_running_requests=num_running_reqs,
+            num_queuing_requests=num_queuing_reqs
+        )
+
+class EngineStatsScraper:
+    def __init__(self, scrape_interval: float):
+        """
+        Args:
+            scrape_interval (float): The interval in seconds 
+                to scrape the metrics.
+
+        Raises:
+            ValueError: if the service discover module is have 
+            not been initialized.
+            
+        """
+        self.service_discovery = GetServiceDiscovery()
+        self.engine_stats: Dict[str, EngineStats] = {}
+        self.engine_stats_lock = threading.Lock()
+
+        self.scrape_interval = scrape_interval
+        self.scrape_thread = threading.Thread(
+            target=self._scrape_worker,
+            daemon=True
+        )
+        self.scrape_thread.start()
+
+    def _scrape_one_endpoint(self, url: str):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            engine_stats = EngineStats.FromVllmScrape(response.text)
+        except Exception as e:
+            logger.error(f"Failed to scrape metrics from {url}: {e}")
+            return None
+        return engine_stats
+
+    def _scrape_metrics(self):
+        collected_engine_stats = {}
+        urls = self.service_discovery.get_engine_urls()
+        logger.info(f"Scraping metrics from {len(urls)} serving engine(s)")
+        for url in urls:
+            engine_stats = self._scrape_one_endpoint(url + "/metrics")
+            if engine_stats:
+                collected_engine_stats[url] = engine_stats
+        
+        with self.engine_stats_lock:
+            old_urls = list(self.engine_stats.keys())
+            for old_url in old_urls:
+                if old_url not in collected_engine_stats:
+                    del self.engine_stats[old_url]
+
+            for url, stats in collected_engine_stats.items():
+                self.engine_stats[url] = stats
+
+    def _scrape_worker(self):
+        while True:
+            self._scrape_metrics()
+            time.sleep(self.scrape_interval)
+
+    def get_engine_stats(self) -> Dict[str, EngineStats]:
+        with self.engine_stats_lock:
+            return self.engine_stats.copy()
+
+def InitializeEngineStatsScraper(
+    scrape_interval: float
+) -> EngineStatsScraper:
+    """
+    Initialize the EngineStatsScraper object. This function should be 
+    called after the service discovery module has been initialized.
+
+    Raises:
+        ValueError: if the service discover module is have 
+            not been initialized
+
+        ValueError: if the EngineStatsScraper object has already been
+            initialized
+    """
+    global _global_engine_stats_scraper
+    if _global_engine_stats_scraper:
+        raise ValueError("EngineStatsScraper object has already been initialized")
+
+    _global_engine_stats_scraper = EngineStatsScraper(scrape_interval)
+    return _global_engine_stats_scraper
+
+def GetEngineStatsScraper() -> EngineStatsScraper:
+    """
+    Get the EngineStatsScraper object
+
+    Raises:
+        ValueError: if the EngineStatsScraper object has not been 
+            initialized
+    """
+    global _global_engine_stats_scraper
+    if not _global_engine_stats_scraper:
+        raise ValueError("EngineStatsScraper object has not been initialized")
+
+    return _global_engine_stats_scraper
+
+if __name__ == "__main__":
+    from service_discovery import InitializeServiceDiscovery, ServiceDiscoveryType
+    import time
+    InitializeServiceDiscovery(ServiceDiscoveryType.K8S, 
+                               namespace = "default", 
+                               port = 8000, 
+                               label_selector = "release=test")
+    time.sleep(1)
+    InitializeEngineStatsScraper(10.0)
+    engine_scraper = GetEngineStatsScraper()
+    while True:
+        engine_stats = engine_scraper.get_engine_stats()
+        print(engine_stats)
+        time.sleep(2.0)
--- router/service_discovery.py
+++ router/service_discovery.py
@@ -1,12 +1,23 @@
 from typing import List, Dict, Optional
 import threading
 import abc
+import enum
 from kubernetes import client, config, watch
 
-
 from log import init_logger
-
 logger = init_logger(__name__)
+
+_global_service_discovery: "Optional[ServiceDiscovery]" = None
+
+def check_pod_ready(container_statuses):
+    if not container_statuses:
+        return False
+    ready_count = sum(1 for status in container_statuses if status.ready)
+    return ready_count == len(container_statuses)
+
+class ServiceDiscoveryType(enum.Enum):
+    STATIC = "static"
+    K8S = "k8s"
 
 class ServiceDiscovery(metaclass = abc.ABCMeta):
     @abc.abstractmethod
@@ -26,11 +37,31 @@ class StaticServiceDiscovery(ServiceDiscovery):
         self.urls = urls
 
     def get_engine_urls(self) -> List[str]:
+        """
+        Get the URLs of the serving engines that are available for
+        querying.
+
+        Returns:
+            a list of engine URLs
+        """
         return self.urls
 
 
 class K8sServiceDiscovery(ServiceDiscovery):
     def __init__(self, namespace: str, port: str, label_selector = None):
+        """
+        Initialize the Kubernetes service discovery module. This module 
+        assumes all serving engine pods are in the same namespace, listening
+        on the same port, and have the same label selector.
+
+        It will start a daemon thread to watch the engine pods and update
+        the url of the available engines.
+
+        Args:
+            namespace: the namespace of the engine pods
+            port: the port of the engines
+            label_selector: the label selector of the engines
+        """
         self.namespace = namespace
         self.port = port
         self.available_engines: Dict[str, str] = {}
@@ -64,47 +95,127 @@ class K8sServiceDiscovery(ServiceDiscovery):
             event_type = event["type"]
             pod_name = pod.metadata.name
             pod_ip = pod.status.pod_ip
-            self._on_engine_update(pod_name, pod_ip, event_type)
+            is_pod_ready = check_pod_ready(pod.status.container_statuses)
+            self._on_engine_update(pod_name, pod_ip, event_type, is_pod_ready)
+
+    def _add_engine(self, engine_name: str, engine_ip: str):
+        logger.info(f"Discovered new serving engine {engine_name} at "
+                    f"{engine_ip}")
+        with self.available_engines_lock:
+            self.available_engines[engine_name] = engine_ip
+
+    def _delete_engine(self, engine_name: str):
+        logger.info(f"Serving engine {engine_name} is deleted")
+        with self.available_engines_lock:
+            del self.available_engines[engine_name]
 
     def _on_engine_update(
             self, 
             engine_name: str,
             engine_ip: Optional[str],
-            event: str
+            event: str,
+            is_pod_ready: bool,
         ) -> None:
         if event == "ADDED":
             if engine_ip is None:
                 return
 
-            with self.available_engines_lock:
-                self.available_engines[engine_name] = engine_ip
+            if not is_pod_ready:
+                return
+
+            self._add_engine(engine_name, engine_ip)
 
         elif event == "DELETED":
             if engine_name not in self.available_engines:
                 return
 
-            with self.available_engines_lock:
-                del self.available_engines[engine_name]
+            self._delete_engine(engine_name)
         
         elif event == "MODIFIED":
             if engine_ip is None:
                 return
 
-            with self.available_engines_lock:
-                self.available_engines[engine_name] = engine_ip
+            if is_pod_ready:
+                self._add_engine(engine_name, engine_ip)
+                return
+
+            if not is_pod_ready and \
+                    engine_name in self.available_engines:
+                self._delete_engine(engine_name)
+                return 
 
     def get_engine_urls(self) -> List[str]:
-        # Use Kubernetes API to get the engine URLs
+        """
+        Get the URLs of the serving engines that are available for
+        querying.
+
+        Returns:
+            a list of engine URLs
+        """
         with self.available_engines_lock:
             return [
                 f"http://{ip}:{self.port}" for ip in self.available_engines.values()
             ]
 
+def InitializeServiceDiscovery(
+        service_discovery_type: ServiceDiscoveryType,
+        *args, **kwargs) -> ServiceDiscovery:
+    """
+    Initialize the service discovery module with the given type and arguments.
+
+    Args:
+        service_discovery_type: the type of service discovery module
+        *args: positional arguments for the service discovery module
+        **kwargs: keyword arguments for the service discovery module
+
+    Returns:
+        the initialized service discovery module
+
+    Raises:
+        ValueError: if the service discovery module is already initialized
+        ValueError: if the service discovery type is invalid
+    """
+    global _global_service_discovery
+    if _global_service_discovery is not None:
+        raise ValueError("Service discovery module already initialized")
+
+    if service_discovery_type == ServiceDiscoveryType.STATIC:
+        _global_service_discovery = StaticServiceDiscovery(*args, **kwargs)
+    elif service_discovery_type == ServiceDiscoveryType.K8S:
+        _global_service_discovery = K8sServiceDiscovery(*args, **kwargs)
+    else:
+        raise ValueError("Invalid service discovery type")
+
+    return _global_service_discovery
+
+def GetServiceDiscovery() -> ServiceDiscovery:
+    """
+    Get the initialized service discovery module.
+
+    Returns:
+        the initialized service discovery module
+
+    Raises:
+        ValueError: if the service discovery module is not initialized
+    """
+    global _global_service_discovery
+    if _global_service_discovery is None:
+        raise ValueError("Service discovery module not initialized")
+
+    return _global_service_discovery
+
 if __name__ == "__main__":
     # Test the service discovery
-    k8s_sd = K8sServiceDiscovery("default", 8000, "release=test")
+    #k8s_sd = K8sServiceDiscovery("default", 8000, "release=test")
+    InitializeServiceDiscovery(ServiceDiscoveryType.K8S, 
+                               namespace = "default", 
+                               port = 8000, 
+                               label_selector = "release=test")
 
+    k8s_sd = GetServiceDiscovery()
     import time
     time.sleep(1)
-    urls = k8s_sd.get_engine_urls()
-    print(urls)
+    while True:
+        urls = k8s_sd.get_engine_urls()
+        print(urls)
+        time.sleep(2)
