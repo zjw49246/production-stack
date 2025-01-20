617866f1d73546fa1950eee774ca861f98edd558
ApostaC <jc4xvyp@outlook.com>
2025-01-20 22:44:17 +0000

    [Add] kubectl installation cmd

--- utils/install-minikube.sh
+++ utils/install-minikube.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install minikube
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 echo "net.core.bpf_jit_harden=0" | sudo tee -a /etc/sysctl.conf
