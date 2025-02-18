45e16f5edb5e83399d298c75f01b5cec9a9773ed
eamin <3352466209@qq.com>
2025-02-17 20:54:24 -0600

    psgke-shellcheck
    Signed-off-by: eamin <3352466209@qq.com>

--- deployment_on_cloud/gke/entry_point.sh
+++ deployment_on_cloud/gke/entry_point.sh
@@ -31,7 +31,12 @@ gcloud beta container --project "$GCP_PROJECT" clusters create "$CLUSTER_NAME" \
   --disk-type "pd-balanced" \
   --disk-size "100" \
   --metadata disable-legacy-endpoints=true \
-  --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+  --scopes "https://www.googleapis.com/auth/devstorage.read_only",\
+    "https://www.googleapis.com/auth/logging.write",\
+    "https://www.googleapis.com/auth/monitoring",\
+    "https://www.googleapis.com/auth/servicecontrol",\
+    "https://www.googleapis.com/auth/service.management.readonly",\
+    "https://www.googleapis.com/auth/trace.append" \
   --max-pods-per-node "110" \
   --num-nodes "1" \
   --logging=SYSTEM,WORKLOAD \
