273f37873fe333b5a15b46ada44952d8cd223349
Shaoting <shaotingf@uchicago.edu>
2025-03-26 14:45:06 -0500

    [CI/Build] Remove sudo requirements in CI/CD (#325)
    * remove sudo in ci pipeline
    
    Signed-off-by: Shaoting-Feng <shaotingf@uchicago.edu>
    
    * fix end of files
    
    Signed-off-by: Shaoting-Feng <shaotingf@uchicago.edu>
    
    ---------
    
    Signed-off-by: Shaoting-Feng <shaotingf@uchicago.edu>

--- .github/port-forward.sh
+++ .github/port-forward.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Ensure the script is run with root privileges
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root (use sudo)."
-   exit 1
-fi
-
 echo "Waiting for all llmstack pods to be in Running state..."
 
 # Save output
@@ -16,14 +10,13 @@ chmod -R 777 "output-$VAR"
 # Print router logs
 POD_NAME=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep '^vllm-deployment-router')
 kubectl wait --for=condition=ready pod/"$POD_NAME" --timeout=120s
-sudo kubectl logs -f "$POD_NAME" 2>&1 | sudo tee "output-$VAR/router.log" &
-
+kubectl logs -f "$POD_NAME" 2>&1 | tee "output-$VAR/router.log" &
 
 # Loop to check if all llmstack-related pods are in the Running state
 while true; do
     # Get all pods containing "vllm" in their name and extract their STATUS column
-    pod_status=$(sudo kubectl get pods --no-headers | grep "vllm" | awk '{print $3}' | sort | uniq)
-    pod_ready=$(sudo kubectl get pods --no-headers | grep "vllm" | awk '{print $2}' | sort | uniq)
+    pod_status=$(kubectl get pods --no-headers | grep "vllm" | awk '{print $3}' | sort | uniq)
+    pod_ready=$(kubectl get pods --no-headers | grep "vllm" | awk '{print $2}' | sort | uniq)
 
     # If the only unique status is "Running", break the loop and continue
     if [[ "$pod_status" == "Running" ]] && [[ "$pod_ready" == "1/1" ]]; then
@@ -36,9 +29,9 @@ while true; do
 done
 
 # Expose router service
-sudo kubectl patch service vllm-router-service -p '{"spec":{"type":"NodePort"}}'
-ip=$(sudo minikube ip)
-port=$(sudo kubectl get svc vllm-router-service -o=jsonpath='{.spec.ports[0].nodePort}')
+kubectl patch service vllm-router-service -p '{"spec":{"type":"NodePort"}}'
+ip=$(minikube ip)
+port=$(kubectl get svc vllm-router-service -o=jsonpath='{.spec.ports[0].nodePort}')
 
 sleep 5
 
