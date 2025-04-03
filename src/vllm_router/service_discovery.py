541ea59e57c6f8dea3968f53aa5e1ba95562b2de
Yuhan Liu <32589867+YuhanLiu11@users.noreply.github.com>
2025-04-03 14:55:00 -0500

    Minor fix in copyright (#345)
    Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

--- src/vllm_router/service_discovery.py
+++ src/vllm_router/service_discovery.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2021 The vLLM Production Stack Team.
+# Copyright 2024-2025 The vLLM Production Stack Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
